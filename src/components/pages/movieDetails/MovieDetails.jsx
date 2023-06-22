81dd886c6f34e4db21d4e89ff4485f643acd4820
MateuszCharysz <charyszek@interia.pl>
2023-06-22 21:50:23 +0200

    it works

--- src/components/pages/movieDetails/MovieDetails.jsx
+++ src/components/pages/movieDetails/MovieDetails.jsx
@@ -2,19 +2,20 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { movieApiLuncher } from 'service/movieApiLuncher';
 import apiUtils from 'service/apiUtils';
-// import MOVIE_ID from 'service/fakeApiByID'; TODO to delate
+
 import AdditionalInfo from 'components/additionalInfo/AdditionalInfo';
 //import PropTypes from 'prop-types' //TODO uncoment if ready
 
 const MovieDetails = props => {
   const { movieId } = useParams();
-  const [movieIdData, setMovieIdData] = useState({});
+  const [movieIdData, setMovieIdData] = useState('');
   const [movieIdDataDetails, setMovieIdDataDetails] = useState({});
-
+  //   console.log(movieId);
+  // console.log(movieApiLuncher(apiUtils.API_ID(movieId)));
   const dataMovieDetails = useCallback(async () => {
     try {
       const answer = await movieApiLuncher(apiUtils.API_ID(movieId));
-      console.log(answer)
+      console.log(answer);
       setMovieIdData(answer.data.id);
       setMovieIdDataDetails(answer.data);
     } catch (err) {
@@ -33,18 +34,24 @@ const MovieDetails = props => {
   return (
     // TODO make button component, poster Path to check, genres with space
     <div>
-      <button type="button">Go back</button>
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${movieIdDataDetails.poster_path}`}
-        alt={`${movieIdDataDetails.tagline}`}
-      />
-      <h2>{movieIdDataDetails.original_title}</h2>
-      <p>User score: {movieIdDataDetails.vote_average}</p>
-      <h3>Overview</h3>
-      <p>{movieIdDataDetails.overview}</p>
-      <h3>Genres</h3>
-      <p>{movieIdDataDetails.genres.map(({ id, name }) => `${name} `)}</p>
-      <AdditionalInfo />
+      {movieIdData !== '' ? (
+        <>
+          <button type="button">Go back</button>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movieIdDataDetails.poster_path}`}
+            alt={`${movieIdDataDetails.tagline}`}
+          />
+          <h2>{movieIdDataDetails.original_title}</h2>
+          <p>User score: {movieIdDataDetails.vote_average}</p>
+          <h3>Overview</h3>
+          <p>{movieIdDataDetails.overview}</p>
+          <h3>Genres</h3>
+          <p>{movieIdDataDetails.genres.map(({ id, name }) => `${name} `)}</p>
+          <AdditionalInfo />
+        </>
+      ) : (
+        <p>smth went wrong</p>
+      )}
     </div>
   );
 };
